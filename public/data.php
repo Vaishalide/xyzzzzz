214e39fcd6c29868d1979639c0caa4f47a0d97f2
isha dalvi <dipeshxyzyadav@gmail.com>
2025-05-24 17:55:54 +0530

    Update data.php

--- public/data.php
+++ public/data.php
@@ -1,19 +1,19 @@
 <?php
 // Only allow access from your specific domain
-$allowed_origin = "https://pwthor.ct.ws";
+//$allowed_origin = "https://pwthor.ct.ws";
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    if ($_SERVER['HTTP_ORIGIN'] === $allowed_origin) {
-        header("Access-Control-Allow-Origin: $allowed_origin");
-        header("Access-Control-Allow-Methods: GET, POST");
-        header("Access-Control-Allow-Headers: Content-Type");
-    } else {
+//if (isset($_SERVER['HTTP_ORIGIN'])) {
+//    if ($_SERVER['HTTP_ORIGIN'] === $allowed_origin) {
+  //      header("Access-Control-Allow-Origin: $allowed_origin");
+//        header("Access-Control-Allow-Methods: GET, POST");
+//        header("Access-Control-Allow-Headers: Content-Type");
+ //   } else {
         // Deny access if origin is not allowed
-        http_response_code(403);
-        echo "Forbidden";
-        exit;
-    }
-}
+  //      http_response_code(403);
+  //      echo "Forbidden";
+  //      exit;
+  //  }
+//}
 header('Content-Type: application/json');// Optional: restrict to your frontend domain
 
 $url = getenv('IFRAME_URL');
